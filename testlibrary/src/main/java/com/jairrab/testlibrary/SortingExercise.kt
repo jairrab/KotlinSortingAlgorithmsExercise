f4fd3d1c6480cd09f2799c23b792dd49be6ef2ea
jairrab <jaybarria@gmail.com>
2019-06-04 10:35:41 +0800

    improve test driver statistics and results gathering

--- testlibrary/src/main/java/com/jairrab/testlibrary/SortingExercise.kt
+++ testlibrary/src/main/java/com/jairrab/testlibrary/SortingExercise.kt
@@ -1,14 +1,21 @@
 package com.jairrab.testlibrary
 
 import com.jairrab.testlibrary.sortalgorithims.*
+import java.util.*
 import java.util.concurrent.ThreadLocalRandom
+import java.util.stream.Collectors
 import kotlin.system.measureNanoTime
 
-private const val ARRAY_SIZE = 15000
+
+private const val ARRAY_SIZE = 10000
 private const val SMALLEST_ELEMENT = -ARRAY_SIZE / 2
 private const val LARGEST_ELEMENT = ARRAY_SIZE / 2
-private const val NUMBER_OF_TEST = 10
+private const val NUMBER_OF_TEST = 11
+private const val WARMUP_RUNS = 3
+private const val SHOW_INPUT_ARRAY = false
+private const val SHOW_OUTPUT_ARRAY = false //show first output array of each algorithm
 
+@Suppress("ConstantConditionIf")
 fun main() {
     var array = IntArray(ARRAY_SIZE)
     println("Array size = $ARRAY_SIZE")
@@ -16,70 +23,82 @@ fun main() {
     when (1) {
         -1 -> array = intArrayOf(1, -1, 5, 4, -3, -5, 3, 1, 1, 1, 4) // custom array
         0 -> for (i in 0 until array.size) array[i] = i //ascending elements
-        1 -> for (i in 0 until array.size) array[i] = randomInt() //random elements
+        1 -> for (i in 0 until array.size) array[i] =
+            ThreadLocalRandom.current().nextInt(SMALLEST_ELEMENT, LARGEST_ELEMENT + 1) //random elements
         2 -> for (i in 0 until array.size) array[i] = array.size - i //descending elements
     }
 
-    //println(array.toList().toString()) //uncomment to print input array
+    if (SHOW_INPUT_ARRAY) println(array.toList().toString())
 
-    val sortAlgorithms = ArrayList<Pair<String, () -> IntArray>>()
+    val sortAlgorithms: MutableList<Pair<String, () -> IntArray>> = ArrayList()
     sortAlgorithms.add(Pair("BuiltInSort", { BuiltInSort().sort(array.copyOf()) }))
     sortAlgorithms.add(Pair("QuickSortEnd", { QuickSortEnd().sort(array.copyOf()) }))
     sortAlgorithms.add(Pair("QuickSortMiddle", { QuickSortMiddle().sort(array.copyOf()) }))
     sortAlgorithms.add(Pair("QuickSortRandom", { QuickSortRandom().sort(array.copyOf()) }))
     sortAlgorithms.add(Pair("MergeSort", { MergeSort().sort(array.copyOf()) }))
-    sortAlgorithms.add(Pair("BubbleSort", { BubbleSort().sort(array.copyOf()) }))
+    sortAlgorithms.add(Pair("BubbleSort", { BubbleSort().sort(array.copyOf()) })) //throws exception for large arrays
     sortAlgorithms.add(Pair("InsertionSort", { InsertionSort().sort(array.copyOf()) }))
 
-    val results = ArrayList<Triple<String, Long, IntArray>>()
+    val results = ArrayList<Result>()
 
     println()
 
-    for (i in 1..NUMBER_OF_TEST) {
+    repeat(WARMUP_RUNS) {
+        sortAlgorithms.shuffle()
+        for (j in sortAlgorithms) j.second()
+    }
+
+    repeat(NUMBER_OF_TEST) { i ->
         sortAlgorithms.shuffle()
 
         println("Test #$i= ${sortAlgorithms.map { it.first }}")
 
         for (j in sortAlgorithms) {
-            val item = results.find { it.first == j.first }
-
-            if (item == null) {
-                var arr = intArrayOf()
-                results.add(
-                    Triple(
-                        first = j.first,
-                        second = measureNanoTime { arr = j.second() },
-                        third = arr
-                    )
+            var arr = intArrayOf()
+            results.add(
+                Result(
+                    name = j.first,
+                    time = measureNanoTime { arr = j.second() } / 1000000.0,
+                    array = arr
                 )
-            } else {
-                var arr = intArrayOf()
-                results.remove(item)
-                results.add(
-                    Triple(
-                        first = j.first,
-                        second = (measureNanoTime { arr = j.second() } + item.second) / 2,
-                        third = arr
-                    )
-                )
-            }
+            )
         }
     }
 
     println()
 
-    results.sortBy { it.second }
+    val correctArray = results.first { it.name == "BuiltInSort" }.array
 
-    for (i in results) {
-        val speed = getPercentageSpeed(results[0].second, i.second)
-        val correctOutput = results.firstOrNull { it.first == "BuiltInSort" }?.third?.contentEquals(i.third)
-        println("${i.second}ns for ${i.first}, $speed% slower. Output equal to built-in sort= $correctOutput")
-        //println(i.third.toList()) //uncomment to print output array
+    val groupedResult = results
+        .groupBy { it.name }
+        .mapValues { entry ->
+            GroupedResult(
+                arrays = entry.value.map { it.array },
+                time = entry.value.sumByDouble { it.time } / NUMBER_OF_TEST,
+                correctness = entry.value.map { it.array.contentEquals(correctArray) })
+        }
+        .entries
+        .stream()
+        .sorted { a, b -> ((a.value.time - b.value.time) * 1000000).toInt() }
+        .map { it }
+        .collect(Collectors.toList())
+
+    val fastestTime = groupedResult[0].value.time
+
+    groupedResult.forEach { i ->
+        val name = i.key
+        val time = String.format("%.3f", i.value.time).toDouble()
+        val speed = String.format("%.1f", getPercentageSpeed(fastestTime, i.value.time))
+        val correctOutput = String.format("%.1f", (i.value.correctness.filter { it }.count() * 100.0) / NUMBER_OF_TEST)
+
+        println("${time}ms for $name, $speed% slower. $correctOutput% correct")
+
+        if (SHOW_OUTPUT_ARRAY) println(i.value.arrays[0].toList())
     }
 }
 
-private fun getPercentageSpeed(reference: Long, subject: Long): Int {
-    return (((subject - reference) / reference.toDouble()) * 100).toInt()
+private fun getPercentageSpeed(reference: Double, subject: Double): Double {
+    return (((subject - reference) / reference) * 100.0)
 }
 
 internal fun swap(array: IntArray, i: Int, j: Int) {
@@ -88,6 +107,14 @@ internal fun swap(array: IntArray, i: Int, j: Int) {
     array[j] = swapTemp
 }
 
-private fun randomInt(): Int {
-    return ThreadLocalRandom.current().nextInt(SMALLEST_ELEMENT, LARGEST_ELEMENT + 1)
-}
+class Result(
+    val name: String,
+    val time: Double,
+    val array: IntArray
+)
+
+class GroupedResult(
+    val time: Double,
+    val arrays: List<IntArray>,
+    val correctness: List<Boolean>
+)
